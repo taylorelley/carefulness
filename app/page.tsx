5b6762c21b264aa5788690f54c2f0b5c363b5a36
taylorelley <48809387+taylorelley@users.noreply.github.com>
2025-07-08 14:30:41 +1200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ export default function CarefulnessKnob() {
                     dataKey="z"
                     name="Cost"
                     domain={[1, 10]}
-                    range={[60, 5000]}
+                    range={[60, 10000]}
                   />
                   <Scatter
                     name="Data Point"
